cb942f26a04847a30476c73157ff37751173d317
amoljore7 <amoljore7@gmail.com>
2020-03-09 12:53:17 +0530

    WIP - Worked on TodoInput componets UI part

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid';
 
 class App extends Component {
 
   componentDidMount() {
     console.log(">>>Inside of Component Did Mount>>");
-    
+
   }
 
   componentWillUnmount() {
-console.log(">>>inside of component Will Mount>>>");
+    console.log(">>>inside of component Will Mount>>>");
   }
   render() {
     return (
-      <div>
-          <TodoInput />
-          <TodoList />
+      <div className="container">
+        <div className="row">
+          <div className="col=10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput />
+            <TodoList />
+          </div>
+        </div>
+
       </div>
     );
   }
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class TodoInput extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <React.Fragment>
-                TodoInput File
+                <div className="card card-body my-3">
+                    <form>
+                        <div className="input-group">
+                            <div className="input-group-prepend">
+                                <div className="input-group-text bg-primary text-white">
+                                    <i className="fas fa-book" />
+                                </div>
+                            </div>
+                            <input type="text" className="form-control text-capitalize" placeholder="add a todo item" />
+                        </div>
+                        <button type="submit" className="btn btn-block btn-primary mt-3"> add item</button>
+                    </form>
+                </div>
             </React.Fragment>
         )
     }
